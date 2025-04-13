09ece5e94c137e436c1eb5ba4e72efb22d02574e
Jaanya Raheja <janyaraheja@gmail.com>
2025-04-13 16:27:59 +0530

    make some changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
         changeOrigin: true,
         secure: false
       }
-    }
+    },
+    host: true,
+    port: 5173,
+    allowedHosts: ['impatientpink.in'],
   }
 })
